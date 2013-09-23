3b9f010c71f9553c558c2651a4252b5ace628c2b
Elorfin <penin.axel@gmail.com>
2013-09-23 15:40:34 +0200

    dd methods to register bundle into Claroline bundle

--- InnovaAngularUIBundle.php
+++ InnovaAngularUIBundle.php
@@ -2,8 +2,21 @@
 
 namespace Innova\AngularUIBundle;
 
+use Claroline\CoreBundle\Library\PluginBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
+use Claroline\KernelBundle\Bundle\ConfigurationProviderInterface;
+use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
 
-class InnovaAngularUIBundle extends Bundle
+class InnovaAngularUIBundle extends Bundle implements AutoConfigurableInterface
 {
+    public function supports($environment)
+    {
+        return true;
+    }
+    
+    public function getConfiguration($environment)
+    {
+        return new ConfigurationBuilder();
+    }
 }
